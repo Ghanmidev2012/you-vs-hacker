4d3c53340102c88f42f2ff6c1d4029e1e48a68e1
Ghanmidev2012 <242857573+Ghanmidev2012@users.noreply.github.com>
2025-12-27 12:31:38 +0100

    Add files via upload

---
+++ you vs hacker.py
@@ -0,0 +1,39 @@
+import time
+print ("you:i enter browser,oh,this game is good!")
+print ("press r to start")
+if input()=="r":
+   time.sleep(1) 
+print ("you:i dowland,but where the game?")
+time.sleep(3)
+print ("hacker:hhh")
+time.sleep(1)
+print ("you:what?")
+time.sleep(1)
+print ("hacker:i will hack you")
+time.sleep(2)
+print ("you:no,plz don't hack me")
+time.sleep(2)
+print ("hacker:ok,but first i make with you a challenge")
+time.sleep(2)
+print ("you:what?")
+time.sleep(2)
+print ("hacker:chalenge is make your computer safe from hacker")
+time.sleep(2)
+print ("yes/no")
+if input()=="yes":
+    print ("you:chaleenge accept")
+elif input()=="no":
+    print ("hacker:ok,i win!")
+print ("hacker:ok,but you have just 1 min")
+time.sleep(2)
+print ("you:ok")
+print("apps")
+hack=input()==""
+print ("write /start")
+if input()=="/kill hack":
+    print ("the hacker take damge for 1%")
+time.sleep  (2)
+print ("hacker:is may turn")
+print ("write /stop")
+if input ()=="/stop":
+    print ("hacker time is finish,you win")
